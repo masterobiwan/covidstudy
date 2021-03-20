9e2b0f3901b3fd3420cbdcf1a97e71f543726414
Benoît Carne <benoit.carne@sigfox.com>
2021-03-20 14:49:35 +0100

    More efficient to use date range

--- lib/covidapi.py
+++ lib/covidapi.py
@@ -39,23 +39,23 @@ class Client:
                 region=region,
             )).text)
 
-    def get_indicator(self, indicator, type, country, region=None, date=None, formatter=None):
-        assert date
+    def get_indicator(self, indicator, type, country, region=None, daterange=None, formatter=None):
+        assert daterange
         if region is None:
-            results = json.loads(requests.get("{url}/api/resources?indicator={indicator}&type={type}&country={country}&date={date}".format(
+            results = json.loads(requests.get("{url}/api/resources?indicator={indicator}&type={type}&country={country}&daterange={daterange}".format(
                 url=self.url,
                 indicator=indicator,
                 type=type,
                 country=country,
-                date=date,
+                daterange=daterange,
             )).text)
         else:
-            results = json.loads(requests.get("{url}/api/resources?indicator={indicator}&type={type}&country={country}&region={region}&date={date}".format(
+            results = json.loads(requests.get("{url}/api/resources?indicator={indicator}&type={type}&country={country}&region={region}&daterange={daterange}".format(
                 url=self.url,
                 indicator=indicator,
                 type=type,
                 country=country,
-                date=date,
+                daterange=daterange,
                 region=region,
             )).text)
         if formatter is None:
