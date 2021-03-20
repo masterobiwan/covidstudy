a621890e1d7219c9c5f6ab205673d8c1c13abe70
Benoît Carne <benoit.carne@sigfox.com>
2021-03-20 14:50:11 +0100

    First 'working' version

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from lib.covidapi import Client
 from pandas import read_json, to_datetime
 import json
+from datetime import timedelta, datetime
+
+
+print("=== Start ===")
 
 apiclient = Client()
 
@@ -8,8 +12,20 @@ apiclient = Client()
 selected_countries = [{"country": "France"}]
 if selected_countries is None:
     countries_data = apiclient.get_countries()["data"]
+    selected_countries = [{"country": "%"}]
 else:
     countries_data = selected_countries
+
+begin_date = "2020-04-23"
+end_date = "2021-03-17"
+range_api = "{begin}-{end}".format(begin=begin_date.replace("-", ""), end=end_date.replace("-", ""))
+selected_dates = []
+begin_datetime = datetime.strptime(begin_date, "%Y-%m-%d")
+end_datetime = datetime.strptime(end_date, "%Y-%m-%d")
+delta = end_datetime - begin_datetime
+for i in range(delta.days + 1):
+    day = begin_datetime + timedelta(days=i)
+    selected_dates.append({"survey_date": day.strftime("%Y%m%d")})
 
 indicators = [
     {"name": "covid", "field": "cli"},
@@ -56,22 +72,19 @@ indicators = [
     {"name": "appointment_tried"},
 ]
 
-selected_dates = None
-
 countries_json = {}
-for country in countries_data:
-    if selected_dates is None:
-        country_dates = apiclient.get_dates(country=country["country"])["data"]
-    else:
-        country_dates = selected_dates
-    for country_date in country_dates:
-        for indicator_dict in indicators:
-            indicator = indicator_dict["name"]
-            indicator_data = apiclient.get_indicator(indicator=indicator, type="daily", country=country["country"], date=country_date["survey_date"])["data"]
+for country in selected_countries:
+    print("Collecting for country {country}".format(country=country["country"]))
+    for indicator_dict in indicators:
+        indicator = indicator_dict["name"]
+        print("Collecting for indicator {indicator}".format(indicator=indicator))
+        indicator_data = apiclient.get_indicator(indicator=indicator, type="daily", country=country["country"], daterange=range_api)["data"]
+        if indicator_data:
             if indicator not in countries_json:
                 countries_json[indicator] = []
             countries_json[indicator].extend(indicator_data)
     
+print("Processing dataframe...")
 primary_keys = ["survey_date", "country"]
 df_countries = read_json(json.dumps(countries_data))
 df_dates = read_json(json.dumps(selected_dates))
@@ -85,9 +98,14 @@ df["date"] = to_datetime(df["survey_date"], format="%Y%m%d").dt.date
 df = df.drop("survey_date", axis="columns")
 rename_dict = {}
 for indicator in indicators:
-    rename_dict["percent_{old}".format(old=indicator["field"])] = "percent_{new}".format(new=indicator["name"])
-    rename_dict["percent_{old}_unw".format(old=indicator["field"])] = "percent_{new}_unw".format(new=indicator["name"])
-    rename_dict["{old}_se".format(old=indicator["field"])] = "{new}_se".format(new=indicator["name"])
-    rename_dict["{old}_se_unw".format(old=indicator["field"])] = "{new}_se_unw".format(new=indicator["name"])
+    if "field" in indicator:
+        rename_dict["percent_{old}".format(old=indicator["field"])] = "percent_{new}".format(new=indicator["name"])
+        rename_dict["percent_{old}_unw".format(old=indicator["field"])] = "percent_{new}_unw".format(new=indicator["name"])
+        rename_dict["{old}_se".format(old=indicator["field"])] = "{new}_se".format(new=indicator["name"])
+        rename_dict["{old}_se_unw".format(old=indicator["field"])] = "{new}_se_unw".format(new=indicator["name"])
 df = df.rename(rename_dict, axis="columns")
+print("Dataframe sample:")
+print("Writing on S3...")
+print(df)
 df.to_parquet("s3://castelnajac-open-data/covidstudy/", partition_cols=["date"])
+print("=== End ===")
